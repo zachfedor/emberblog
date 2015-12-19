3ee0d5a37081ec410080ae8d850b8cd8f01c80e4
Zach Fedor <zach@theindustrialresolution.com>
2015-12-19 09:07:11 -0500

    create nested post routes

---
+++ app/routes/posts/post.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model(params) {
+        console.log(params);
+        return this.store.findRecord('post', params.post_id);
+    },
+});
